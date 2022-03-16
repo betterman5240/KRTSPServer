485092d295ea96ba2f541bd547be7a5005551c83
RyanHsu <betterman5240@gmail.com>
2022-03-16 18:17:05 +0800

    Fixe issue, changed the header file for the linux kernel 5.0

--- accept.c
+++ accept.c
@@ -25,7 +25,9 @@ Accept connections
 #include "prototypes.h"
 #include "sysctl.h"
 #include "proxy.h"
-#include <linux/smp_lock.h>
+//Ryan added this code.
+#include <linux/hardirq.h>
+//#include <linux/smp_lock.h>
 #include <linux/inet.h>
 
 int AcceptConnections(const int CPUNR, struct socket *Socket)
@@ -54,12 +56,13 @@ int AcceptConnections(const int CPUNR, struct socket *Socket)
 	   the allocation of a new socket. (Which doesn't seem to be 
 	   used anyway)
 	*/
-   	if (Socket->sk->tp_pinfo.af_tcp.accept_queue==NULL)
+	//remove these code, Ryan remove
+   	/*if (Socket->sk->tp_pinfo.af_tcp.accept_queue==NULL)
 	{
 		return 0;
-	}
+	}*/
 	
-                KRTSPROXYD_OUT(KERN_INFO "accept_queue != NULL\n");
+    KRTSPROXYD_OUT(KERN_INFO "accept_queue != NULL\n");
 	error = 0;	
 	while (error>=0)
 	{
@@ -72,8 +75,9 @@ int AcceptConnections(const int CPUNR, struct socket *Socket)
 		NewSock->type = Socket->type;
 		NewSock->ops = Socket->ops;
 		
-		
-	       error = Socket->ops->accept(Socket, NewSock, O_NONBLOCK);
+			// after linux kernel 5.0 accept (sock,newsock,type, bool kern)
+	       //error = Socket->ops->accept(Socket, NewSock, O_NONBLOCK);
+			error = Socket->ops->accept(Socket, NewSock, O_NONBLOCK, true);
 		
 
 		if (error<0)
@@ -82,7 +86,9 @@ int AcceptConnections(const int CPUNR, struct socket *Socket)
 			sock_release(NewSock);
 			break;
 		}
-		if (NewSock->sk->state==TCP_CLOSE)
+		//after linux kernel 5.0, struct sock remove the "state" member
+		//if (NewSock->sk->state==TCP_CLOSE)
+		if(NewSock->sk->sk_state==TCP_CLOSE)
 		{
 			sock_release(NewSock);
 			continue;
@@ -96,8 +102,9 @@ int AcceptConnections(const int CPUNR, struct socket *Socket)
               }
 
 		NewSession->client_skt = NewSock;
-
-		err = NewSock->ops->getname(NewSock, (struct sockaddr *)&skaddr, &len, 1);
+		//after linux kernel, the getname function removed the length member.
+		//err = NewSock->ops->getname(NewSock, (struct sockaddr *)&skaddr, &len, 1);
+		err=NewSock->ops->getname(NewSock, (struct sockaddr *)&skaddr, 1);
 		if(err!=0){
 		  KRTSPROXYD_OUT(KERN_ERR "sorry, getname calling failed!\n");
 		  sock_release(NewSock);
@@ -111,7 +118,9 @@ int AcceptConnections(const int CPUNR, struct socket *Socket)
 	       KRTSPROXYD_OUT(KERN_INFO "add new session to session queue!\n"); 	
 	       
 		init_waitqueue_entry(&NewSession->sleep,current);
-		add_wait_queue(NewSock->sk->sleep,&(NewSession->sleep));
+		//after linux kernel, the sock struct removed the sleep member.
+		//add_wait_queue(NewSock->sk->sleep,&(NewSession->sleep)); Ryan remove these code.
+		add_wait_queue(sk_sleep(NewSock->sk),&(NewSession->sleep));
 		threadinfo[CPUNR].RtspSessionQueue = NewSession;
 		atomic_inc(&ConnectCount);
               atomic_inc(&gNumUsers);
